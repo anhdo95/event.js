08a9252b253ed6d92ad40f7edf728cdfda374b56
Anh Do <do.dinh.anh@linecorp.com>
2021-10-08 21:50:16 +0700

    Add helpers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
+const isObject = (v) => v !== null && typeof v === 'object' && !Array.isArray(v)
+
 class EventJS {
   constructor() {
     this.events = {}
   }
 
-  once() {
-    console.log('once');
+  once(type, handler) {
+    this.events[type] = this.events[type] || {
+      once: true,
+      handler,
+    }
+    return this
   }
 
-  on() {
-    console.log('on');
+  on(type, handler) {
+    this.events[type] = (this.events[type] || []).concat(handler)
+    return this
   }
 
-  off() {
-    console.log('off');
+  off(type) {
+    if (type === undefined) {
+      this.events = {}
+    } else {
+      this.events[type] = []
+    }
+    return this
   }
 
-  trigger() {
-    console.log('trigger');
+  trigger(type) {
+    if (this.events[type]) {
+      if (this.events[type].once) {
+        this.events[type].handler()
+        delete this.events[type]
+      } else {
+        this.events[type].map((handler) => handler())
+      }
+
+    }
+    return this
   }
 }
+
+class EventBus extends EventJS {
+  constructor() {
+    super()
+    this.on('click', () => alert('Click!'))
+      .on('scroll', () => alert('Scroll!'))
+      .once('mouseup', () => alert('Mouse up!'))
+      .once('mouseup', () => alert('Mouse up again!'))
+  }
+
+  dispatch() {
+    this.off('scroll')
+      .trigger('click')
+      .trigger('click')
+      .trigger('scroll')
+      .trigger('mouseup')
+      .trigger('mouseup')
+  }
+}
+
+const bus = new EventBus()
+bus.dispatch()
 
 export default EventJS
